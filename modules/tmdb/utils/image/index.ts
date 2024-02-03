bcc733251bcfe7ebd11e4e666f9d6db2bcc4ebf5
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-03 19:03:30 +0100

    fix: always return string from tmdb image utils

--- modules/tmdb/utils/image/index.ts
+++ modules/tmdb/utils/image/index.ts
@@ -2,36 +2,39 @@ import { BackdropSize, LogoSize, ProfileSize, StillSize } from "./sizes";
 
 const baseUrl = "https://image.tmdb.org/t/p/";
 
-export function getOriginalImage(image: string | undefined) {
-  if (!image) return;
+export function getOriginalImage(image: string | undefined): string {
+  if (!image) return "";
   return `${baseUrl}/original/${image}`;
 }
 
 export function getBackdropImage(
   image: string | undefined,
   backdropSize: BackdropSize = "original",
-) {
-  if (!image) return;
+): string {
+  if (!image) return "";
   return `${baseUrl}/${backdropSize}/${image}`;
 }
 
 export function getLogoImage(
   image: string | undefined,
   logoSize: LogoSize = "original",
-) {
-  if (!image) return;
+): string {
+  if (!image) return "";
   return `${baseUrl}/${logoSize}/${image}`;
 }
 
 export function getProfileImage(
   image: string | undefined,
   profileSize: ProfileSize,
-) {
-  if (!image) return;
+): string {
+  if (!image) return "";
   return `${baseUrl}/${profileSize}/${image}`;
 }
 
-export function getStillImage(image: string | undefined, stillSize: StillSize) {
-  if (!image) return;
+export function getStillImage(
+  image: string | undefined,
+  stillSize: StillSize,
+): string {
+  if (!image) return "";
   return `${baseUrl}/${image}/${stillSize}`;
 }
