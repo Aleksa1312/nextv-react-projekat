0e66b1ee8ed6fb36369ee1ab29a798c0d253bc33
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-06 16:23:07 +0100

    feat: add loading image component

---
+++ modules/ui/loading-image.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { LoaderIcon } from "lucide-react";
+import Image, { ImageProps } from "next/image";
+import { useState } from "react";
+
+export default function LoadingImage({
+  src,
+  width,
+  height,
+  alt,
+  className,
+  ...rest
+}: ImageProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  function onFinishLoading() {
+    setLoading(false);
+  }
+
+  return (
+    <div
+      className={cn(
+        `max-w-sm rounded-[var(--radius)] bg-secondary`,
+        className,
+        loading && `relative flex h-fit w-fit items-center justify-center`,
+      )}
+    >
+      {loading && (
+        <LoaderIcon className="absolute left-0 right-0 m-auto h-fit w-fit animate-spin" />
+      )}
+      <Image
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+        className={cn(className, loading && "opacity-0")}
+        onLoadingComplete={onFinishLoading}
+        {...rest}
+      />
+    </div>
+  );
+}
