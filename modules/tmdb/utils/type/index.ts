3cfbe9aab3e695fe9056c5b53052b21f909d171f
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-09 11:29:37 +0100

    feat: account for response no media_type key case

--- modules/tmdb/utils/type/index.ts
+++ modules/tmdb/utils/type/index.ts
@@ -1,13 +1,27 @@
-import { MovieResult, PersonResult, TvResult } from "moviedb-promise";
+import {
+  MovieResponse,
+  MovieResult,
+  PersonResult,
+  ShowResponse,
+  TvResult,
+} from "moviedb-promise";
 
 export function isTvResult(object: any): object is TvResult {
-  return object.media_type === "tv";
+  return object.media_type === "tv" || object.name;
 }
 
 export function isMovieResult(object: any): object is MovieResult {
-  return object.media_type === "movie";
+  return object.media_type === "movie" || object.title;
 }
 
 export function isPersonResult(object: any): object is PersonResult {
   return object.media_type === "person";
+}
+
+export function isShowResponse(object: any): object is ShowResponse {
+  return object.media_type === "tv" || object.name;
+}
+
+export function isMovieResponse(object: any): object is MovieResponse {
+  return object.media_type === "movie" || object.title;
 }
