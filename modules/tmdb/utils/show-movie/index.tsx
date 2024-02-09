e46ad86ceeb3c727a2121562f11a0fdf649373d4
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-09 11:30:18 +0100

    feat: add showMovie getter utils

---
+++ modules/tmdb/utils/show-movie/index.tsx
@@ -0,0 +1,24 @@
+import { MovieResponse, ShowResponse } from "moviedb-promise";
+import { isMovieResponse } from "../type";
+
+export function getNameTitle(
+  showMovie: MovieResponse | ShowResponse | null,
+): string {
+  if (!showMovie) return "";
+
+  const isMovie = isMovieResponse(showMovie);
+
+  return (isMovie ? showMovie.title : showMovie.name) || "";
+}
+
+export function getShowMovieDate(
+  showMovie: MovieResponse | ShowResponse | null,
+): string {
+  if (!showMovie) return "";
+
+  console.log(showMovie);
+
+  const isMovie = isMovieResponse(showMovie);
+
+  return (isMovie ? showMovie.release_date : showMovie.first_air_date) || "";
+}
