1322fb9812dcc856b91c8cca3bdab8dec5642dcd
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-09 15:48:44 +0100

    style: make logo smaller on smaller devices

--- modules/header/components/logo/index.tsx
+++ modules/header/components/logo/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import useScreenWidth from "@/modules/common/hooks/useScreenWidth";
 import { buttonVariants } from "@/modules/ui/button";
 import Link from "next/link";
 import { HTMLAttributes } from "react";
@@ -6,16 +9,20 @@ import { HTMLAttributes } from "react";
 interface LogoProps extends HTMLAttributes<HTMLAnchorElement> {}
 
 export default function Logo({ className, ...rest }: LogoProps) {
+  const { isBreakpoint } = useScreenWidth();
+
+  const logoText = !isBreakpoint("md") ? "NEXTV" : "N";
+
   return (
     <Link
       href="/"
       className={buttonVariants({
         variant: "ghost",
-        className: cn("text-xl font-bold xl:text-2xl", className),
+        className: cn("text-xl font-bold md:flex xl:text-2xl", className),
       })}
       {...rest}
     >
-      NEXTV
+      {logoText}
     </Link>
   );
 }
