49692f306ad1e1fa86f42fc8c4d43fdf3cd23cad
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-09 15:48:09 +0100

    fix: make link absolute instead of relative

--- modules/tmdb/utils/href/index.ts
+++ modules/tmdb/utils/href/index.ts
@@ -1,11 +1,11 @@
 export function getTvHref(id: number | undefined) {
   if (!id) return "/";
 
-  return `details/tv/${id}`;
+  return `/details/tv/${id}`;
 }
 
 export function getMovieHref(id: number | undefined) {
   if (!id) return "/";
 
-  return `details/movie/${id}`;
+  return `/details/movie/${id}`;
 }
