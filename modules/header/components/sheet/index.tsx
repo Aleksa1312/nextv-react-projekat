cad340b770ed6754a7af33f1a67a3d28e3a6ff30
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-02-09 12:22:32 +0100

    feat: add profile-settings to mobile nav

--- modules/header/components/sheet/index.tsx
+++ modules/header/components/sheet/index.tsx
@@ -10,19 +10,21 @@ import {
 import { ArrowRightIcon, MenuIcon } from "lucide-react";
 import { links } from "../navigation/links";
 import HeaderLink from "../link";
+import { ThemeDropdown } from "@/modules/theme/components/dropdown";
+import HeaderUser from "../user";
 
 export default function NavigationSheet() {
   return (
     <Sheet>
       <SheetTrigger>
-        <MenuIcon></MenuIcon>
+        <MenuIcon />
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="min-w-0">
         <SheetHeader>
           <SheetTitle>Navigation</SheetTitle>
           <SheetDescription>Where would you like to go?</SheetDescription>
         </SheetHeader>
-        <nav className="mt-10 flex h-full w-full flex-col items-center gap-2">
+        <nav className="mt-10 flex w-full flex-col items-center gap-2">
           {links.map(({ href, title }, id) => (
             <SheetClose asChild key={id}>
               <HeaderLink href={href}>
@@ -32,6 +34,10 @@ export default function NavigationSheet() {
             </SheetClose>
           ))}
         </nav>
+        <div className="absolute bottom-0 mr-5 flex w-full min-w-0 flex-row items-center justify-start gap-3 bg-background py-10">
+          <HeaderUser />
+          <ThemeDropdown />
+        </div>
       </SheetContent>
     </Sheet>
   );
